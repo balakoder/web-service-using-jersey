76625b4b69a91f880cfe199e6f404a3a9c18195f
devuser <karjule128@gmail.com>
2017-09-24 15:47:17 +0530

    commit with code

---
+++ src/main/java/com/bala/rest/MyService.java
@@ -0,0 +1,22 @@
+package com.bala.rest;
+
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/jersey")
+public class MyService {
+
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Welcome : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Welcome  : Rest web services in java !</h2>
+<a href="rest/jersey/bala"> Click here</a>
+</body>
+</html>
